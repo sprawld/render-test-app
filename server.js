b130771737a7434f308dfae296d5c1f7535eb98a
plmcmillan <peter.mcmillan@mobileum.com>
2023-01-07 16:51:36 +0000

    simple redis text load/save

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const Redis = require("ioredis");
 
+const { REDIS_URL = `localhost:6379`, PORT = 3001 } = process.env;
+
+const app = express();
+const db = new Redis(REDIS_URL);
+
+app.use(express.json())
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/gettxt', (req, res) => {
+
+    db.get('current').then(txt => {
+        res.send({txt})
+    });
+});
+
+app.post('/settxt', (req, res) => {
+
+    console.log(`req`,req.body);
+    let {txt} = req.body;
+    console.log(`req ${txt}`, req.body)
+    db.set('current', txt);
+
+    res.send({});
+});
+
+
+
+app.listen(PORT, () => console.log(`Example app listening on port http://localhost:${PORT}!`));
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,49 @@
 <script>
-	export let name;
+	
+	import {onMount} from 'svelte';
+
+	let text = 'Hello World';
+	
+	onMount(() => {
+		console.log(`loading`);
+		load();		
+	});
+
+	function load() {
+		fetch('/gettxt').then(res => res.json()).then(data => {
+
+			console.log(`got`, data);
+
+			text = data.txt;
+		})
+	}
+
+	function save() {
+		fetch('/settxt', {
+			method: 'POST', 
+			mode: 'cors', // no-cors, *cors, same-origin
+			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: 'same-origin', // include, *same-origin, omit
+			headers: {
+			'Content-Type': 'application/json'
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			redirect: 'follow', // manual, *follow, error
+			referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		
+		body: JSON.stringify({txt: text})}).then(() => {
+			console.log('saved');
+		})
+	}
+
 </script>
 
 <main>
-	<h1>Hello World</h1>
+	<div>
+		<button on:click={save}>Save</button>
+	</div>
+
+	<textarea bind:value={text}></textarea>
 </main>
 
 <style>
